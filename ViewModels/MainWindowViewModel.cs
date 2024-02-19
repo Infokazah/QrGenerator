699ddb40f08b230b61ccf6fff8d593083ff33125
Влад <gaimer433@gmail.com>
2024-02-19 13:58:59 +0300

    Добавление иконок на QrCode

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -4,10 +4,14 @@ using QRCoder;
 using QrGenerator.infrastructure;
 using System;
 using System.Drawing;
+using System.Security.Policy;
 using System.Windows;
 using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using WpfBaseLyb;
+using Brush = System.Drawing.Brush;
 using Color = System.Windows.Media.Color;
 
 
@@ -16,6 +20,21 @@ namespace QrGenerator.ViewModels
     internal class MainWindowViewModel : ViewModelBase
     {
         private ImageSource _qrCodeImage;
+
+        private ImageSource _qrCodeIcon;
+
+        public ImageSource QrCodeIcon
+        {
+            get => _qrCodeIcon;
+
+            set
+            {
+                _qrCodeIcon = value;
+                OnPropertyChanged(nameof(QrCodeIcon));
+            }
+        }
+
+        private string QrIconPath;
 
         private int _qrSize;
 
@@ -75,26 +94,58 @@ namespace QrGenerator.ViewModels
         {
             if (str is string stroke)
             {
-
                 QRCodeData qrCodeData = _qrGenerator.CreateQrCode(stroke, QRCodeGenerator.ECCLevel.Q);
                 QRCode qrCode = new QRCode(qrCodeData);
-                Bitmap QRCodeBitMap = qrCode.GetGraphic(QrSize, ColorMediaToColorDrawingConverter.ConvertToDrawingColor(BackColor), ColorMediaToColorDrawingConverter.ConvertToDrawingColor(FrontColor), true);
-                QRCodeImage = Imaging.CreateBitmapSourceFromHBitmap(QRCodeBitMap.GetHbitmap(), 
-                    IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                Bitmap QRCodeBitMap = qrCode.GetGraphic(100, GlobalConverter.ConvertToDrawingColor(BackColor), GlobalConverter.ConvertToDrawingColor(FrontColor), true);
+
+
+                using (Graphics graphics = Graphics.FromImage(QRCodeBitMap))
+                {
+                    Bitmap logo = new Bitmap(QrIconPath);
+                    int logoSizeX = QRCodeBitMap.Width / 6;
+                    int logoSizeY = QRCodeBitMap.Height / 6;
+                    int logoPositionX = (QRCodeBitMap.Width - logoSizeX) / 2;
+                    int logoPositionY = (QRCodeBitMap.Height - logoSizeY) / 2;
+
+                    graphics.DrawImage(logo, new Rectangle(logoPositionX, logoPositionY, logoSizeX, logoSizeY));
+                }
+
+                QRCodeImage = Imaging.CreateBitmapSourceFromHBitmap(QRCodeBitMap.GetHbitmap(),
+                        IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
             }
         }
 
-        public RegularCommand DownLoadIconCode { get; }
-        private bool CanDownLoadIconExecute(object p) => true;
+        public SimpleCommand DownLoadIconCode { get; }
+        private bool CanDownLoadIconExecute() => true;
 
         private void DownLoadIconExecute()
         {
-           
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    string selectedFile = openFileDialog.FileName;
+                    QrCodeIcon = GlobalConverter.ConvertStringToImageSource(selectedFile);
+                    QrIconPath = openFileDialog.FileName;
+                }
+                else
+                {
+                    MessageBox.Show("Файлы не выбраны.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка: " + ex.Message);
+            }
+
         }
         public MainWindowViewModel() 
         {
             _qrGenerator = new QRCodeGenerator();
             GenerateQrCode = new RegularCommand(GenerateQrCodeExecute, CanGenerateQrCodeExecute);
+            DownLoadIconCode = new SimpleCommand(DownLoadIconExecute, CanDownLoadIconExecute);
         }
     }
 }
---
+++ infrastructure/GlobalConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace QrGenerator.infrastructure
+{
+    internal static class GlobalConverter
+    {
+        public static System.Drawing.Color ConvertToDrawingColor(this System.Windows.Media.Color wpfColor)
+        {
+            return System.Drawing.Color.FromArgb(wpfColor.A, wpfColor.R, wpfColor.G, wpfColor.B);
+        }
+
+        public static ImageSource ConvertStringToImageSource(string imagePath)
+        {
+            try
+            {
+                BitmapImage imageSource = new BitmapImage();
+
+                imageSource.BeginInit();
+                imageSource.UriSource = new Uri(imagePath, UriKind.RelativeOrAbsolute);
+                imageSource.EndInit();
+
+                return imageSource;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка при загрузке изображения: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
